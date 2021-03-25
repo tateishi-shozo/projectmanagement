c733e0929fdddec9869e6a406abe2ac7f8448566
shozo <tateishi.shozo@gmail.com>
2021-03-25 01:48:17 +0000

    アサインフロント変更済

--- resources/views/admin/project/assign.blade.php
+++ resources/views/admin/project/assign.blade.php
@@ -37,49 +37,70 @@
         </table>
     </div>
     <div class="m-auto">
-        <h2 class="border border-secondary bg-primary text-white" style="padding:10px;">アサイン可能</h2>
-            <div class="card-deck">
-                @foreach($users as $user)
-                    <form action="{{ action('Admin\ProjectController@record') }}" method="post">
-                        @csrf
-                        <div class="card m-2" style="width:10rem;">
-                            <img class="card-img" src="{{ $user->image }}" alt="Card image" height="150">
-                            <div class="card-body">
-                                <h4 class="card-title">{{$user->name }}</h4>
-                                <input type="hidden" name="start_date" value="{{ $project->start_date }}">
-                                <input type="hidden" name="end_date" value="{{ $project->end_date }}">
-                                <input type="hidden" name="user_id" value="{{ $user->user_id }}">
-                                <input type="hidden" name="id" value="{{ $project->id }}">
-                                <input type="submit" class="btn btn-primary" value="追加">
-                            </div>
-                        </div>
-                    </form>
+        <h2 class="border bg-primary text-white" style="padding:10px;">アサイン可能</h2>
+    </div>
+    <div class="col-md-12 mx-auto">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ユーザー</th>
+                    <th>名前</th>
+                    <th>追加</th>
+                </tr>
+            </thead>
+            <tbody>
+                 @foreach($users as $user)
+                        <tr>
+                            <th><img src="{{ $user->image }}" height="100" width="100"></th>
+                            <td>{{ $user->name }}</td>
+                            <td>
+                                <form action="{{ action('Admin\ProjectController@record') }}" method="post">
+                                @csrf
+                                    <input type="hidden" name="start_date" value="{{ $project->start_date }}">
+                                    <input type="hidden" name="end_date" value="{{ $project->end_date }}">
+                                    <input type="hidden" name="user_id" value="{{ $user->user_id }}">
+                                    <input type="hidden" name="id" value="{{ $project->id }}">
+                                    <input type="submit" class="btn btn-primary" value="追加">
+                                </form>
+                            </td>
+                        </tr>
                 @endforeach
-            </div>
+            </tbody>
+        </table>
     </div>
     <div class="m-auto">
-        <h2 class="border border-secondary bg-secondary text-white" style="padding:10px;">アサイン終了</h2>
-            <div class="card-deck">
-                @foreach($notassingusers as $notassinguser)
-                    <form action="{{ action('Admin\ProjectController@remove') }}" method="post">
-                        @csrf
-                            <div class="card m-2" style="width:10rem;">
-                                    <img class="card-img" src="{{ $notassinguser->image }}" alt="Card image" height="150">
-                                    <div class="card-img-overlay">
-                                        <h2 class="card-text text-center m-3">{{ $notassinguser->project_name }}</h2>
-                                    </div>
-                                <div class="card-body">
-                                    <h4 class="card-title">{{$notassinguser->name }}</h4>
+        <h2 class="border bg-secondary text-white" style="padding:10px;">アサイン可能</h2>
+    </div>
+    <div class="col-md-12 mx-auto">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ユーザー</th>
+                    <th>名前</th>
+                    <th>アサイン済</th>
+                    <th>削除</th>
+                </tr>
+            </thead>
+            <tbody>
+                 @foreach($notassingusers as $notassinguser)
+                        <tr>
+                            <th><img src="{{ $notassinguser->image }}" height="100" width="100"></th>
+                            <td>{{ $notassinguser->name }}</td>
+                            <td>{{ $notassinguser->project_name }}</td>
+                            <td>
+                                <form action="{{ action('Admin\ProjectController@remove') }}" method="post">
+                                @csrf
                                     <input type="hidden" name="start_date" value="{{ $project->start_date }}">
                                     <input type="hidden" name="end_date" value="{{ $project->end_date }}">
                                     <input type="hidden" id="user_id" name="user_id" value="{{ (int)$notassinguser->user_id }}">
                                     <input type="hidden" id="id" name="id" value="{{ $project->id }}">
                                     <input type="submit" class="btn btn-primary" value="削除">
-                                </div>
-                            </div>
-                    </form>
+                                </form>
+                            </td>
+                        </tr>
                 @endforeach
-            </div>
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
